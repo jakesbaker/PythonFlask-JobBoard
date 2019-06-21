f70dd0ebf61c44c014165d71f6d846849c66a2cb
Jake Baker <jake@onestepnetworks.com>
2019-06-21 16:09:33 -0700

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -43,5 +43,6 @@ def job(job_id):
 
 @app.route('/employer/<employer_id>')
 def employer(employer_id):
-    job = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], 'SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id], single=True)
-    return render_template('employer.html', employer=employer, reviews=reviews)
+    employer = execute_sql('SELECT * FROM employer WHERE id=?', [employer_id], single=True)
+    job = execute_sql('SELECT job.id, job.title, job.description, job.salary FROM job JOIN employer ON employer.id = job.employer_id WHERE employer.id = ?', [employer_id])
+    return render_template('employer.html', employer=employer, jobs=jobs)
